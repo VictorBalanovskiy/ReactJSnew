43f168f1f8f4a86ab531ee9bf16782bc10e8d544
Виктор Эдуардович Балановский <82959239+VictorBalanovskiy@users.noreply.github.com>
2021-09-13 20:45:46 +0700

    Add files via upload

--- src/Store/Chats/reducer.js
+++ src/Store/Chats/reducer.js
@@ -1,17 +1,36 @@
 import { ADD_CHAT_ACTION } from "./constans";
+import { DELETE_CHAT_ACTION } from "./constans";
+import { getId } from "../../utils";
+import { getCounter } from "../../utils";
 
 const initialState = {
-  chatsList: [],
+  chatList: [],
 };
+
+const counter = getCounter();
 
 export const chatsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_CHAT_ACTION: {
       return {
         ...state,
-        chats: [...state.chatsList, { ...action.payload }],
+        chatList: [
+          ...state.chatList,
+          {
+            id: `id${getId()}`,
+            name: `Chat ${counter()}`,
+          },
+        ],
       };
     }
+    case DELETE_CHAT_ACTION:
+      const newChatList = state.chatList.filter(
+        (chat) => action.payload.id !== chat.id
+      );
+      return {
+        ...state,
+        chatList: [...newChatList],
+      };
     default:
       return state;
   }
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,14 +1,30 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { chatsReducer } from "./Chats/reducer";
 import { messageReducer } from "./Messages/reducer";
 import { profileReducer } from "./Profile/reducer";
+import { middleware } from "../Middlewares";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+  key: "persistConfig",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  profile: profileReducer,
+  chats: chatsReducer,
+  messages: messageReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(
-  combineReducers({
-    profile: profileReducer,
-    chats: chatsReducer,
-    messages: messageReducer,
-  }),
-  applyMiddleware(thunk)
+  persistedReducer,
+  composeEnhancers(applyMiddleware(middleware, thunk))
 );
+
+export const persistor = persistStore(store);
